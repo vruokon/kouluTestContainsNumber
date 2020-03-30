fff51f9c3a8352b7fcb66a94f02baa531e6e3e71
Viljami Ruokonen <vruokonen@gmail.com>
2020-03-30 09:20:29 +0300

    Added function displayResult

--- js/contains_number.js
+++ js/contains_number.js
@@ -1,3 +1,14 @@
 const containsNumber = (numbers, aNumber) => {
     return numbers.indexOf(aNumber) >= 0 ? true : false;
 }
+
+const displayResult = () => {
+    let aNumber = 11;
+    let numbers = [1, 4, 2, 5, 6, 3, 5, 3, 2];
+
+    if (containsNumber(numbers, aNumber)) {
+        return `Array contains the number ${aNumber}`;
+    } else {
+        return `Array doesn't contain the number ${aNumber}`;
+    }
+}
